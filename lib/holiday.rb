5d9a4e878c8f399db6130c7caffb6ead7f0f69cd
Nora Vogt <noraevogt@gmail.com>
2018-11-25 18:25:49 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,7 +79,6 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holidays|
       if holidays.values.include?("BBQ")
         holidays_with_bbqs << holidays
-      end
     end
   end
 
