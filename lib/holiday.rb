63dc0fcaa381086cc5a88cb007168dc0d599174b
Nora Vogt <noraevogt@gmail.com>
2018-11-25 17:29:47 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,7 +50,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  winter_supplies = holiday_hash[:winter].values
+  winter_supplies = holiday_hash[:winter].values.flatten
   binding.pry
 
 
