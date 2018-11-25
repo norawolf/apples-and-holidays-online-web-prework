43f544e4add7f32528db94c058a5a5dc405a25cd
Nora Vogt <noraevogt@gmail.com>
2018-11-25 18:23:43 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,5 +74,11 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  holidays_with_bbqs = []
+
+  holiday_hash.each do |season, holidays|
+    holidays.each do |holiday, items|
+      
+    
 
 end
