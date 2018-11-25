4a3b436b5f2a8e0199f673c69f4b561bbcd4d7a9
Nora Vogt <noraevogt@gmail.com>
2018-11-25 18:11:36 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts "#{season.to_s.capitalize!}:"
     holidays.each do |holiday, items|
-      puts "  #{holiday.to_s.split("_").collect {|name| name.capitalize!}.join(' ') }"
+      puts "  #{holiday.to_s.split("_").collect {|name| name.capitalize!}.join(' ')}"
       #puts "  #{holiday.to_s.split("_").collect {|name| name.capitalize!}.join(' ')}: #{items.join(", ")}"
     end
   end
